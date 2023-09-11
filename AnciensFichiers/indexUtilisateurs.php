8dc853c60d1b82244ddfa193472519271e973659
Eric Marin ME <2130033@cmontmorency.qc.ca>
2023-09-11 18:12:07 -0400

    Retrait des anciens fichiers inutiles

--- AnciensFichiers/indexUtilisateurs.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'Modele.php';
-try {
-    $utilisateurs = getUtilisateurs();
-    require 'vueUtilisateurs.php';
-} catch (Exception $e) {
-    $msgErreur = $e->getMessage();
-    require 'vueErreur.php';
-}
