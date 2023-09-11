fe9d26c1969c014fe58c527679ce88586821f8ee
Eric Marin ME <2130033@cmontmorency.qc.ca>
2023-09-11 18:11:10 -0400

    Commit initial MVC Procédural

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ try {
         {
             if (isset($_GET['id']))
             {
-                // intval renvoie la valeur numérique du paramètre ou 0 en cas d'échec
+                // intval renvoie la valeur numérique du paramètre ou 0 en cas d'échec 
                 $id = intval($_GET['id']);
                 if ($id !=  0)
                 {
